eb0f598dca94310d40e492e5c9def2722f075220
Lynn Hao <lihao@ucsd.edu>
2016-02-18 02:51:23 +0000

    added coin flip

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,22 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function(req,res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num > 0.5) {
+		projects["grid"] = false;
+		res.render('index', projects);
+	} else {
+		res.redirect('/grid');
+	}
+};
+
+/*exports.view = function(req, res){
   	res.render('index', projects);
   	projects["grid"] = false;
-};
+};*/
 
 
 exports.viewGrid = function(req, res){
