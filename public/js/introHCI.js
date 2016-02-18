4aaec95007f2a5bda6c95bd3607ceb7f1f022cc6
Lynn Hao <lihao@ucsd.edu>
2016-02-18 02:34:55 +0000

    Lab 7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".thumbnail .likeBtn").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	ga("send","event","like","click");
 }
